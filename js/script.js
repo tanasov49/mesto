54a3f84fcd0e4aa0f06e72eb0f0603ee0eff12fe
tanasov49 <tanasov49@gmail.com>
2022-03-24 22:08:13 +0200

    updates 24.03

--- js/script.js
+++ js/script.js
@@ -1,29 +1,46 @@
 let popupOpen = document.querySelector('.profile__click-profile');
+
 let popupClose = document.querySelector('.popup-form__close-btn');
+
 let popup = document.querySelector('.popup');
+
 popupOpen.addEventListener('click', function() {
     popup.classList.add('popup_opened');
 })
-popupClose.addEventListener('click', function() {
-    popup.classList.remove('popup_opened');
-})
+
+
+
 let popupForm = document.querySelector('.popup-form');
+
 let textName = document.querySelector('.popup-form__name');
+
 let textSkill =document.querySelector('.popup-form__skill');
+
 let profileTitle = document.querySelector('.profile__title');
+
 let profileSubtitle = document.querySelector('.profile__subtitle');
+
 let saveForm = document.querySelector('.popup-form__save-btn');
+
 textName.value = profileTitle.textContent;
 textSkill.value = profileSubtitle.textContent;
+
 function formSubmit(evt) {
     evt.preventDefault();
     profileTitle.textContent = textName.value;
-    profileSubtitle.textContent = textSkill.value;
+    profileSubtitle.textContent = textSkill.value; 
 }
+popupClose.addEventListener('click', function() {
+    textName.value = profileTitle.textContent;
+    textSkill.value = profileSubtitle.textContent;
+    popup.classList.remove('popup_opened');
+})
+
 popupForm.addEventListener('submit', formSubmit); 
 saveForm.addEventListener('click', function() {
     popup.classList.remove('popup_opened');
 })
+
 
 
 
